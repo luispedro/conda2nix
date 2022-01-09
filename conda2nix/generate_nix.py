0853c15d5958a1f4456875967554a83585b9584a
Luis Pedro Coelho <luis@luispedro.org>
2022-01-09 13:28:18 +0100

    ENH Change how building is performed
    Use a separate build.sh script, which allows for more flexibility

--- conda2nix/generate_nix.py
+++ conda2nix/generate_nix.py
@@ -20,11 +20,7 @@ stdenv.mkDerivation rec {{
 
   '';
 
-  buildPhase = ''
-
-  {buildPhase}
-
-  '';
+  builder = ./build.sh;
 
   phases = [ "unpackPhase" "buildPhase" ];
 
@@ -52,6 +48,19 @@ TEMPLATE_SRC_URL = '''
     url = "{url}";
     {hash_method} = "{hash_value}";
   }};
+'''
+
+PRE_BUILD_SH = '''
+set -e
+
+source $stdenv/setup
+
+PREFIX=$out
+mkdir $out
+
+unpackPhase
+cd $sourceRoot
+
 '''
 
 def norm_requirement(r):
@@ -119,53 +128,59 @@ def extract_source(src):
         raise KeyError(f'Could not find hash in {src}')
     return TEMPLATE_SRC_URL.format(url=url, hash_value=hash_value, hash_method=hash_method)
 
-def as_dependencies(deps, sep, add_stdenv=False):
+def as_dependencies(deps, sep, is_top=False):
     if deps is None:
         return ''
     dependencies = [conda2nix_requirement(r)
                 for r in deps]
     dependencies = [dep for dep in dependencies if dep]
-    if add_stdenv:
+    if is_top:
+        dependencies = [dep.split('.')[0] for dep in dependencies]
         dependencies.append('stdenv')
         dependencies.append('lib')
         dependencies.append('fetchurl')
-    return sep.join(dependencies)
+    return sep.join(set(dependencies))
 
 
 def extract_build(pk):
-    bfile = '../../recipes/{}/build.sh'.format(pk['package']['name'])
+    bfile = 'bioconda-recipes/recipes/{}/build.sh'.format(pk['package']['name'])
+    base = None
     if path.exists(bfile):
         with open(bfile) as ifile:
-            return ifile.read()
+            base = ifile.read()
     elif 'script' in pk['build']:
         script = pk['build']['script']
         if type(script) == str:
-            return 'python ' + pk['build']['script']
+            base = 'python ' + pk['build']['script']
         elif type(script) == list:
-            return '\n'.join(script)
-    raise KeyError(f'Could not extract buildPhase from {pk}')
+            base = '\n'.join(script)
+    if base is None:
+        raise KeyError(f'Could not extract buildPhase from {pk}')
+    return PRE_BUILD_SH + base
 
 
 def normalize_build(b):
-    return b \
-            .replace('${PREFIX}', '$out') \
-            .replace('$PREFIX', '$out')
+    return b
 
 def generate_nix(pk, dirname):
     dependencies = as_dependencies(all_requirements_of(pk), ', ', True)
     buildInputs = as_dependencies(pk.get('requirements', {}).get('build', []), ' ')
     nativeBuildInputs = as_dependencies(pk.get('requirements', {}).get('run', []), ' ')
-    buildPhase = normalize_build(extract_build(pk))
+    build_sh = normalize_build(extract_build(pk))
 
     pname = pk['package']['name']
     version = pk['package']['version']
     checkPhase = ''
-    description = pk['about']['summary']
+    description = pk['about']['summary'] \
+                .replace('"', r'\"') \
+                .replace('$', r'\$')
     homepage = pk['about']['home']
     src = extract_source(pk['source'])
     odir = f'{dirname}/{pk["package"]["name"]}'
     os.makedirs(odir, exist_ok=True)
     with open(f'{odir}/default.nix', 'wt') as out:
         out.write(TEMPLATE.format(**locals()))
+    with open(f'{odir}/build.sh', 'wt') as out:
+        out.write(build_sh)
     return pk['package']['name']
 

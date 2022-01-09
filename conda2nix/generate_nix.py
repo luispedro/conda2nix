00ea0bc2791e708efee98d5c78df8be6ec96a8ac
Luis Pedro Coelho <luis@luispedro.org>
2022-01-09 13:27:13 +0100

    MIN Quote webpage
    Sometimes, it includes characters like '#' which do not get parsed correctly otherwise

--- conda2nix/generate_nix.py
+++ conda2nix/generate_nix.py
@@ -31,7 +31,7 @@ stdenv.mkDerivation rec {{
 
   meta = with lib; {{
     description = "{description}";
-    homepage = {homepage};
+    homepage = "{homepage}";
     platforms = platforms.all;
     maintainers = with maintainers; [ luispedro ];
   }};
